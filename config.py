f328cc2d82cf79a8d7be82d566c09bb9097a54c0
Tian Peng <ianpeng97@hotmail.com>
2022-02-28 20:20:09 -0500

    new db URI

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ['SECRET_KEY']
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'].replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
